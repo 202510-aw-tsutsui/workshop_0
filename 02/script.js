7bbe80b99c82dda50b028ec1cb415994beccb84f
202510-aw-tsutsui <202510_aw_tsutsui@x-ide.sakura.ne.jp>
2026-03-12 14:26:24 +0900

    02ヘッダー、フッター

---
+++ 02/script.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const pagerLinks = document.querySelectorAll(".pager a");
+
+  pagerLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      pagerLinks.forEach((a) => a.classList.remove("active"));
+      link.classList.add("active");
+    });
+  });
+});
