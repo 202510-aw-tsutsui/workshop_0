6135ca9df804b3125003f612c1eaeba27383f720
202510-aw-tsutsui <202510_aw_tsutsui@x-ide.sakura.ne.jp>
2026-03-12 16:27:21 +0900

    お問い合わせボタン修正

---
+++ 07/script.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const reserveBtn = document.querySelector(".reserve-btn");
+
+  if (reserveBtn) {
+    reserveBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      alert("予約ページへ進みます。");
+    });
+  }
+});
